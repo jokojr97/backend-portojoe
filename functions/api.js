4156568c684bee777d99db1059850213152761ba
jokojr97 <joko.riyadi97@gmail.com>
2022-06-03 20:18:30 +0700

    update cors

--- functions/api.js
+++ functions/api.js
@@ -8,6 +8,13 @@ const Portofolio = require('../model/portofolio');
 const app = express()
 const router = express.Router();
 
+app.use((req, res, next) => {
+    req.setHeader('Access-Control-Allow-Origin', '*');
+    req.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    req.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 router.get('/', (req, res) => {
     Portofolio.find().then((portof) => {
         res.send(portof)
